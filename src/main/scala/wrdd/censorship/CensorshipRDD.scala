f89f1b762b4479455708030674d6f3cec8486341
Pawel Szulc <paul.szulc@gmail.com>
2016-09-01 20:46:41 +0200

    Simplify CensorshipRDD construction

--- src/main/scala/wrdd/censorship/CensorshipRDD.scala
+++ src/main/scala/wrdd/censorship/CensorshipRDD.scala
@@ -3,7 +3,7 @@ package wrdd.censorship
 import org.apache.spark._
 import org.apache.spark.rdd._
 
-class CensorshipRDD(prev: RDD[String]) extends RDD[String](prev.context, List(new OneToOneDependency(prev))) {
+class CensorshipRDD(prev: RDD[String]) extends RDD[String](prev) {
 
   override def compute(split: Partition, context: TaskContext): Iterator[String] = ???
 
