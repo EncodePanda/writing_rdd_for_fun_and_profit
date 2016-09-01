a986f05279dc04d5eb133650107f5ca095c231a1
Pawel Szulc <paul.szulc@gmail.com>
2016-09-01 18:56:25 +0200

    Provide (not yet implemented) methods for RandomRDD

--- src/main/scala/wrdd/random/RandomRDD.scala
+++ src/main/scala/wrdd/random/RandomRDD.scala
@@ -3,8 +3,10 @@ package wrdd.random
 import org.apache.spark._
 import org.apache.spark.rdd._
 
-
 class RandomRDD(_sc: SparkContext, maxSize: Int = 100, numPartitions: Int = 2) extends RDD[Int](_sc, Nil) {
 
+  override def compute(split: Partition, context: TaskContext): Iterator[Int] = ???
+
+  override protected def getPartitions: Array[Partition] = ???
 
 }
