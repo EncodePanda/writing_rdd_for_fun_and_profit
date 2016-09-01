d5b42c88b51585ae78c67581657f1651d52dbd88
Pawel Szulc <paul.szulc@gmail.com>
2016-09-01 18:51:06 +0200

    Add RandomRDD with contructor (does not compile)

---
+++ src/main/scala/wrdd/random/RandomRDD.scala
@@ -0,0 +1,10 @@
+package wrdd.random
+
+import org.apache.spark._
+import org.apache.spark.rdd._
+
+
+class RandomRDD(_sc: SparkContext, maxSize: Int = 100, numPartitions: Int = 2) extends RDD[Int](_sc, Nil) {
+
+
+}
