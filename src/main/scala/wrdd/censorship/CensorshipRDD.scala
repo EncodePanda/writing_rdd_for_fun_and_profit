50fcec7ca2a7d52194542416665b1b7056d2b023
Pawel Szulc <paul.szulc@gmail.com>
2016-09-01 20:52:03 +0200

    Add compute implementation for CensorshipRDD

--- src/main/scala/wrdd/censorship/CensorshipRDD.scala
+++ src/main/scala/wrdd/censorship/CensorshipRDD.scala
@@ -5,7 +5,8 @@ import org.apache.spark.rdd._
 
 class CensorshipRDD(prev: RDD[String]) extends RDD[String](prev) {
 
-  override def compute(split: Partition, context: TaskContext): Iterator[String] = ???
+  override def compute(split: Partition, context: TaskContext): Iterator[String] =
+    firstParent[String].compute(split, context).map(str => str.replace("Hadoop", "******"))
 
   override protected def getPartitions: Array[Partition] =
     firstParent[String].partitions
