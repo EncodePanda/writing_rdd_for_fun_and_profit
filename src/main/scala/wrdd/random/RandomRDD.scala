56c4c9bc941d6c3c011254d2441e2cffca3feb97
Pawel Szulc <paul.szulc@gmail.com>
2016-09-01 19:08:00 +0200

    Provide implementation of compute method of RandomRDD

--- src/main/scala/wrdd/random/RandomRDD.scala
+++ src/main/scala/wrdd/random/RandomRDD.scala
@@ -3,9 +3,12 @@ package wrdd.random
 import org.apache.spark._
 import org.apache.spark.rdd._
 
+import scala.util.Random._
+
 class RandomRDD(_sc: SparkContext, maxSize: Int = 100, numPartitions: Int = 2) extends RDD[Int](_sc, Nil) {
 
-  override def compute(split: Partition, context: TaskContext): Iterator[Int] = ???
+  override def compute(split: Partition, context: TaskContext): Iterator[Int] =
+     (1 to nextInt(maxSize / numPartitions)).map(n => nextInt()).toList.toIterator
 
   override protected def getPartitions: Array[Partition] =  {
     val array = new Array[Partition](numPartitions)
