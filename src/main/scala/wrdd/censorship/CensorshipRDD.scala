4700e5539cdfcda66901ff839758637700d10482
Pawel Szulc <paul.szulc@gmail.com>
2016-09-01 20:46:04 +0200

    Add constructor to CensorshipRDD

--- src/main/scala/wrdd/censorship/CensorshipRDD.scala
+++ src/main/scala/wrdd/censorship/CensorshipRDD.scala
@@ -3,7 +3,7 @@ package wrdd.censorship
 import org.apache.spark._
 import org.apache.spark.rdd._
 
-class CensorshipRDD extends RDD[String]() {
+class CensorshipRDD(prev: RDD[String]) extends RDD[String](prev.context, List(new OneToOneDependency(prev))) {
 
   override def compute(split: Partition, context: TaskContext): Iterator[String] = ???
 
