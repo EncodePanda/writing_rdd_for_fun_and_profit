960f9abd1eb70a22b1435b388bfbf09bdf23b4e9
Pawel Szulc <paul.szulc@gmail.com>
2016-09-01 20:08:32 +0200

    Extend Main template with initialized SparkContext

--- src/main/scala/wrdd/Main.scala
+++ src/main/scala/wrdd/Main.scala
@@ -1,7 +1,14 @@
 package wrdd
 
+import org.apache.spark._
+
 object Main extends App {
 
   println("sandbox")
+  val sc = new SparkContext(new SparkConf().setMaster("local[*]").setAppName("Main"))
+
+  
+
+  sc.stop()
 
 }
