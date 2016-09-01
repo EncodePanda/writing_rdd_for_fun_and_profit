88c00c364ded796e7a78a88e4aba9e638d058a6f
Pawel Szulc <paul.szulc@gmail.com>
2016-09-01 20:55:21 +0200

    Extends RDD[String] with censor method

--- src/main/scala/wrdd/censorship/CensorshipRDD.scala
+++ src/main/scala/wrdd/censorship/CensorshipRDD.scala
@@ -3,6 +3,12 @@ package wrdd.censorship
 import org.apache.spark._
 import org.apache.spark.rdd._
 
+object CensorshipRDD {
+  implicit class StringRDDOps(rdd: RDD[String]) {
+    def censor() = new CensorshipRDD(rdd)
+  }
+}
+
 class CensorshipRDD(prev: RDD[String]) extends RDD[String](prev) {
 
   override def compute(split: Partition, context: TaskContext): Iterator[String] =
