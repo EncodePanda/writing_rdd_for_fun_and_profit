6bc17531ed18effb822a3287b87960f002dd1df6
Pawel Szulc <paul.szulc@gmail.com>
2016-09-02 21:30:42 +0200

    Add collectLegal implementation (does not work)

--- src/main/scala/wrdd/censorship/CensorshipRDD.scala
+++ src/main/scala/wrdd/censorship/CensorshipRDD.scala
@@ -4,6 +4,9 @@ import org.apache.spark._
 import org.apache.spark.rdd._
 
 object CensorshipRDD {
+
+  val heresy = "Hadoop"
+
   implicit class StringRDDOps(rdd: RDD[String]) {
     def censor() = new CensorshipRDD(rdd)
   }
@@ -11,8 +14,13 @@ object CensorshipRDD {
 
 class CensorshipRDD(prev: RDD[String]) extends RDD[String](prev) {
 
+  def collectLegal(): Array[String] = {
+    val results = context.runJob(this, (it: Iterator[String]) => it.filter(!_.toLowerCase().contains(CensorshipRDD.heresy)).toArray)
+    Array.concat(results: _*)
+  }
+
   override def compute(split: Partition, context: TaskContext): Iterator[String] =
-    firstParent[String].compute(split, context).map(str => str.replace("Hadoop", "******"))
+    firstParent[String].compute(split, context).map(str => str.replace(CensorshipRDD.heresy, "******"))
 
   override protected def getPartitions: Array[Partition] =
     firstParent[String].partitions
