6d7c8953c43a942630832ea9dffb807a807e5d66
Pawel Szulc <paul.szulc@gmail.com>
2016-09-02 21:36:29 +0200

    Fix collectLegal action

--- src/main/scala/wrdd/censorship/CensorshipRDD.scala
+++ src/main/scala/wrdd/censorship/CensorshipRDD.scala
@@ -6,6 +6,7 @@ import org.apache.spark.rdd._
 object CensorshipRDD {
 
   val heresy = "Hadoop"
+  val mask = "******"
 
   implicit class StringRDDOps(rdd: RDD[String]) {
     def censor() = new CensorshipRDD(rdd)
@@ -15,12 +16,12 @@ object CensorshipRDD {
 class CensorshipRDD(prev: RDD[String]) extends RDD[String](prev) {
 
   def collectLegal(): Array[String] = {
-    val results = context.runJob(this, (it: Iterator[String]) => it.filter(!_.toLowerCase().contains(CensorshipRDD.heresy)).toArray)
+    val results = context.runJob(this, (it: Iterator[String]) => it.filter(!_.toLowerCase().contains(CensorshipRDD.mask)).toArray)
     Array.concat(results: _*)
   }
 
   override def compute(split: Partition, context: TaskContext): Iterator[String] =
-    firstParent[String].compute(split, context).map(str => str.replace(CensorshipRDD.heresy, "******"))
+    firstParent[String].compute(split, context).map(str => str.replace(CensorshipRDD.heresy, CensorshipRDD.mask))
 
   override protected def getPartitions: Array[Partition] =
     firstParent[String].partitions
